ce516e18c4ae6bc7ec2a6e3baec88d0623d6af48
Terence Ge <Terencegehui@yahoo.com>
2016-06-17 01:09:08 +0800

    add customization for text and shadow color

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import style from './style.css'
 
 class FancyText extends React.Component {
   constructor(props, context) {
     super(props, context)
+  	let style = document.createElement('style')
+  	style.appendChild(document.createTextNode(''))
+  	document.head.appendChild(style)
+    this.state = {
+      timingFunctions: ['linear', 'ease-in', 'ease-out', 'ease-in-out', 'ease'],
+      styleSheet: style.sheet,
+      textColor: this.props.textColor || '#F9F6F5',
+      shadowColor: this.props.textColor || '#fff'
+    }
   }
 
   getRandomInt(min, max) {
@@ -12,18 +20,38 @@ class FancyText extends React.Component {
   }
 
   renderLetters(text) {
+    let keyframes =
+    `@keyframes fading {
+      from {
+        color: rgba(255,255,255,0);
+        text-shadow: 0 0 .5em ${this.state.shadowColor};
+      }
+      to {
+        color: ${this.state.textColor};
+        text-shadow: 0 0 .1em ${this.state.shadowColor};
+      }
+    }`
+    this.state.styleSheet.insertRule(keyframes, this.state.styleSheet.cssRules.length)
+
     let letters = []
-    let timingFunctions = ['linear', 'ease-in', 'ease-out', 'ease-in-out', 'ease']
     for (let i = 0; i < text.length; i++) {
       let time = this.getRandomInt(30,60) / 10
-      let timingFunction = timingFunctions[this.getRandomInt(0,5)]
+      let timingFunction = this.state.timingFunctions[this.getRandomInt(0,5)]
       let animationStyle = {
+        WebkitAnimationName: 'fading',
+        animationName: 'fading',
         WebkitAnimationDuration: time + 's',
         animationDuration: time + 's',
+        WebkitAnimationDelay: '0s',
+        animationDelay: '0s',
+        WebkitAnimationIterationCount: 'infinite',
+        animationIterationCount: 'infinite',
+        WebkitAnimationDirection: 'alternate',
+        animationDirection: 'alternate',
         WebkitAnimationTimingFunction: timingFunction,
         animationTimingFunction: timingFunction,
       }
-      letters.push(<span key={i} className={style.charAnimation + ' char' + i} style={animationStyle}>{text[i]}</span>)
+      letters.push(<span key={i} className={'char' + i} style={animationStyle}>{text[i]}</span>)
     }
     return letters
   }
